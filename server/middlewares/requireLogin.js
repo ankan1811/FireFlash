9fed4dad5a317024389d4e94dadd55e3c12e2a91
Ankan Pal <ankan1811@gmail.com>
2020-11-15 17:28:50 +0530

    Add files via upload

---
+++ server/middlewares/requireLogin.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../config/keys')
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+//Middleware is a code which modifies the incoming request before it reaches to actual routehandler.It is always passed inside app.use()
+//Then it will apply for all the routes.If we want middleware to be executed only for a particular route then we can use:
+//   app.get('/about',middleware,(req,res)=>{
+
+//})
+module.exports = (req,res,next)=>{//Middleware function 
+    const {authorization} = req.headers//export it so that other files can access it
+    //authorization === Bearer ewefwegwrherhe
+    //if authorization is not there(Bearer (token))
+    if(!authorization){
+        //unauthorized to access it
+       return res.status(401).json({error:"you must be logged in"})
+    }
+    const token = authorization.replace("Bearer ","")
+    jwt.verify(token,JWT_SECRET,(err,payload)=>{
+        //callback with error and payload
+        if(err){
+         return   res.status(401).json({error:"you must be logged in"})
+        }
+
+        const {_id} = payload//destructuring _id from payload
+        User.findById(_id).then(userdata=>{
+            req.user = userdata//req.user contains all user details
+            next()
+        })
+        
+        
+    })
+}

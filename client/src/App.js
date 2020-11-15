f1b354ac410e4fcabf5ee326c7862426bb117329
Ankan Pal <ankan1811@gmail.com>
2020-11-15 15:17:58 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,80 @@
+//Don,t think npx create-react-app client
+//npm install react-router-dom
+import React,{useEffect,createContext,useReducer,useContext} from 'react';
+import NavBar from './components/Navbar'
+import "./App.css"
+import {BrowserRouter,Route,Switch,useHistory} from 'react-router-dom'
+import Home from './components/screens/Home'
+import Signin from './components/screens/SignIn'
+import Profile from './components/screens/Profile'
+import Signup from './components/screens/Signup'
+import CreatePost from './components/screens/CreatePost'
+import {reducer,initialState} from './reducers/userReducer'
+import UserProfile from './components/screens/UserProfile'
+import SubscribedUserPosts from './components/screens/SubscribesUserPosts'
+import Reset from './components/screens/Reset'
+import NewPassword from './components/screens/Newpassword'
+export const UserContext = createContext()
+
+
+const Routing = ()=>{
+  const history = useHistory()
+  const {state,dispatch} = useContext(UserContext)//if user has closed application but still logged in state is getting changed 
+  useEffect(()=>{                                 //which should not occur as he should be able to access the protected resource
+                                                    // the next time he opens it without logging in again.
+    const user = JSON.parse(localStorage.getItem("user"))//parse string back to object
+    if(user){
+      dispatch({type:"USER",payload:user})//if  user is present i.e. logged in redirect to home screen
+    }else{
+      if(!history.location.pathname.startsWith('/reset'))//if user is not present redirect to login screen
+           history.push('/signin')
+    }
+  },[])
+  return(
+    <Switch>
+      <Route exact path="/" >
+      <Home />
+      </Route>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/create">
+        <CreatePost/>
+      </Route>
+      <Route path="/profile/:userid">
+        <UserProfile />
+      </Route>
+      <Route path="/myfollowingpost">
+        <SubscribedUserPosts />
+      </Route>
+      <Route exact path="/reset">
+        <Reset/>
+      </Route>
+      <Route path="/reset/:token">
+        <NewPassword />
+      </Route>
+      
+    </Switch>
+  )
+}
+
+function App() {
+  const [state,dispatch] = useReducer(reducer,initialState)
+  return (
+    <UserContext.Provider value={{state,dispatch}}>
+    <BrowserRouter>
+      <NavBar />
+      <Routing />
+      
+    </BrowserRouter>
+    </UserContext.Provider>
+  );
+}
+
+export default App;

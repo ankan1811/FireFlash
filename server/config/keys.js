91fca95fd80e95810ea1c817610a145f8ff52a56
Ankan Pal <ankan1811@gmail.com>
2021-02-11 19:05:02 +0530

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV==='production'){
+if(process.env.NODE_ENV==='production'){ //NODE_ENV is protected in .env file
     module.exports = require('./prod')
 }else{
     module.exports = require('./dev')

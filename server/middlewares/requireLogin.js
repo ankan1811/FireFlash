35be8202edd865f218c94f3af604c98cc5af0487
Ankan Pal <ankan1811@gmail.com>
2021-02-11 20:17:16 +0530

    Update requireLogin.js

--- server/middlewares/requireLogin.js
+++ server/middlewares/requireLogin.js
@@ -16,7 +16,7 @@ module.exports = (req,res,next)=>{//Middleware function
        return res.status(401).json({error:"you must be logged in"})
     }
     const token = authorization.replace("Bearer ","")
-    jwt.verify(token,JWT_SECRET,(err,payload)=>{
+    jwt.verify(token,JWT_SECRET,(err,payload)=>{//Generate a token by Bearer string. Check if it is the same token that was created in jwt.
         //callback with error and payload
         if(err){
          return   res.status(401).json({error:"you must be logged in"})

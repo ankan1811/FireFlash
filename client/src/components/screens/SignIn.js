da311bb408bcd4a5b28ea60f3ed6b10e6f5cadd7
Ankan Pal <ankan1811@gmail.com>
2022-10-30 16:44:55 +0530

    Proper css

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -64,7 +64,7 @@ const SignIn  = ()=>{ //state,dispatch was passed in userContext provider in app
             value={password}
             onChange={(e)=>setPasword(e.target.value)}
             />
-            <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+            <button className="btn waves-effect waves-light #f50057 pink accent-3"
             onClick={()=>PostData()}
             >
                 Login
